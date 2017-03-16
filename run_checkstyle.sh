0811d6f72a5192c5ac713db50c95b4fea2130b1d
christiankolb <christiankolb@gmx.net>
2017-03-16 11:30:09 +0100

    Update run_checkstyle.sh

--- run_checkstyle.sh
+++ run_checkstyle.sh
@@ -1,2 +1,2 @@
 
-java -jar /usr/checkstyle/checkstyle-7.6-all.jar -c /usr/checkstyle/google_checks.xml /src/sample/java/project
+java -jar /usr/checkstyle/checkstyle-7.6-all.jar -c /usr/checkstyle/google_checks.xml /var/lib/go-agent/pipelines/Another_Test/src/sample/java/project
