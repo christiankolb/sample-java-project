8ef0b6816ed3e61a8c37877fcc0ae0bcab5d8b43
christiankolb <christiankolb@gmx.net>
2017-10-19 04:23:04 +0200

    Update SampleJavaProject.java

--- src/sample/java/project/SampleJavaProject.java
+++ src/sample/java/project/SampleJavaProject.java
@@ -49,6 +49,7 @@ public class SampleJavaProject implements Runnable {
             }
         } catch (ParameterException e) {
             System.err.println("error: " + e.getMessage());
+            e.printStacktrace()
             new JCommander(new SampleJavaProject()).usage();
             System.exit(-1);
         }
